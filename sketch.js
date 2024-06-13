56314e192f43e44a532487a8602ca24f323b5b2a
noonbg3br <gu.paulavassari@gmail.com>
2024-06-13 14:05:27 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,25 @@
+lavra;
+
+function setup() {
+  createCanvas(400, 400);
+
+  palavra = palavraAleatoria();
+  
+}
+
+function palavraAleatoria() {
+  
+  let palavras = ["Caminhante", "Caminho", "Caminha"];
+  
+  return random(palavras);
+}
+
+function inicializaCores() {
+  background("white");
+  fill("black");
+  textSize(64);
+  textAlign(CENTER, CENTER);
+}
+
+function palavraParcial(minimo, maximo) {
+  let quantidade = m
